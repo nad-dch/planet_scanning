0f9da5e3aaa950e07c34d9f54bd1e520c707f229
Konstantina Dachlythra <konstantina.dachlythra@fysik.su.se>
2019-10-14 14:38:56 +0200

    adding more files

---
+++ noise.py
@@ -0,0 +1,10 @@
+import nump as np
+
+def white_noise(rms=1.0):
+
+	return np.randn(rms)
+
+def onef_noise(rms=1.0, alpha=1.0, beta=1.0):
+
+	pass
+	
---
+++ test_script1.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# read planet timelines
+# generate beam time using beam_model.py and planet_model.py
+# add noise
+# plot timeline
