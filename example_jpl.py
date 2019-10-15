ad2958af8854419146fb5a9024436ab442826d45
Jon <jegudmunds@gmail.com>
2019-10-15 22:49:01 +0100

    minor changes

--- example_jpl.py
+++ example_jpl.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 import numpy as np
 import pickle
@@ -9,7 +10,7 @@ opj = os.path.join
 
 def get_planet_timelines(planet_id, start_date, end_date,
 						time_step):
-	
+
 	#planet_id can be either a 3digit number from the IAU
 	#catalogue either the barycenter of the planet
 	#insert date in form YYYYMMDD
@@ -20,18 +21,19 @@ def get_planet_timelines(planet_id, start_date, end_date,
 	end_date = str(end_date)[:4]+'-'+str(end_date)[4:6]+'-'+str(end_date)[6:]
 	time_step = str(time_step)+'d'
 
-	Epochs = {'start':start_date,'stop':end_date,	
+	Epochs = {'start':start_date,'stop':end_date,
 			'step':time_step}
 
-	obj = Horizons(id = str(planet_id), id_type = 'majorbody', 
-					location = None, epochs = Epochs)
+	obj = Horizons(id = str(planet_id), id_type = 'majorbody',
+		location = None, epochs = Epochs)
+
 	eph = obj.ephemerides()
 	ra = np.array(eph['RA'])
 	dec = np.array(eph['DEC'])
 	t = np.array(eph['datetime_jd'])
 	timelines = {'t':t, 'ra':ra, 'dec':dec}
 
-	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'),timelines)
+	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'), timelines)
 
 
 def call_horizons(verbose=False, time_interval='1h'):
@@ -55,7 +57,7 @@ def call_horizons(verbose=False, time_interval='1h'):
     # Printing out some useful debutting statements
     if verbose:
 
-        print(eph['datetime_str'], eph['RA'], eph['DEC'])        
+        print(eph['datetime_str'], eph['RA'], eph['DEC'])
         print(type(eph['RA'][0]))
         print(type(eph['RA']))
         print(eph.columns)
--- planet_model.py
+++ planet_model.py
@@ -1,16 +1,25 @@
+import scipy
 import numpy as np
 import astropy
 from astroquery.jplhorizons import Horizons
-import os
+
+h = scipy.constants.h
 
 def brightness(T , d):
+    '''
+    Insert useful comment
+    '''
 
-	h = 6.2606014e-034
-	#v= 
-	B = 2*h*(v**2)*k*T/(c**2)*4*np.pi*(d**2)
+    h = 6.2606014e-034
+    B = 2*h*(v**2)*k*T/(c**2)*4*np.pi*(d**2)
 
-	return B
+    return B
 
+def main():
 
+    T = 2.7
+    d = 10.
 
-	
+if __name__ == '__main__':
+
+    main()
