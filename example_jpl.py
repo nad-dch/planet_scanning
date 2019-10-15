de70460493eaef2c13da2a53cc9c534c89328af1
nad-dch <55231643+nad-dch@users.noreply.github.com>
2019-10-15 16:34:59 +0200

    Update example_jpl.py

--- example_jpl.py
+++ example_jpl.py
@@ -5,6 +5,34 @@ import astroquery
 import astropy
 import datetime
 from astroquery.jplhorizons import Horizons
+opj = os.path.join
+
+def get_planet_timelines(planet_id, start_date, end_date,
+						time_step):
+	
+	#planet_id can be either a 3digit number from the IAU
+	#catalogue either the barycenter of the planet
+	#insert date in form YYYYMMDD
+	filefolder ='/location_of_directory/'
+
+	#convert the dates into the correct form YYYY-MM-DD
+	start_date = str(start_date)[:4]+'-'+str(start_date)[4:6]+'-'+str(start_date)[6:]
+	end_date = str(end_date)[:4]+'-'+str(end_date)[4:6]+'-'+str(end_date)[6:]
+	time_step = str(time_step)+'d'
+
+	Epochs = {'start':start_date,'stop':end_date,	
+			'step':time_step}
+
+	obj = Horizons(id = str(planet_id), id_type = 'majorbody', 
+					location = None, epochs = Epochs)
+	eph = obj.ephemerides()
+    ra = np.array(eph['RA'])
+    dec = np.array(eph['DEC'])
+    t = np.array(eph['datetime_jd'])
+    timelines = {'t':t, 'ra':ra, 'dec':dec}
+
+	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'),timelines)
+
 
 def call_horizons(verbose=False, time_interval='1h'):
 
