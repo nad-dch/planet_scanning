df853816dca99e3c85b2b0b1511e1e5e5eff5e0f
nad-dch <55231643+nad-dch@users.noreply.github.com>
2019-10-15 16:41:47 +0200

    Update planet_model.py

--- planet_model.py
+++ planet_model.py
@@ -11,52 +11,6 @@ def brightness(T , d):
 
 	return B
 
-def get_planet_timelines(planet_id, start_date, end_date,
-						time_step):
-	
-	#planet_id can be either a 3digit number from the IAU
-	#catalogue either the barycenter of the planet
-	#insert date in form YYYYMMDD
-	filefolder ='/home/nadia/git_repos/planet_scanning/'
-	opj = os.path.join
-
-	#convert the dates into the correct form YYYY-MM-DD
-	start_date = str(start_date)[:4]+'-'+str(start_date)[4:6]+'-'+str(start_date)[6:]
-	end_date = str(end_date)[:4]+'-'+str(end_date)[4:6]+'-'+str(end_date)[6:]
-	time_step = str(time_step)+'d'
-
-	Epochs = {'start':start_date,'stop':end_date,	
-			'step':time_step}
-
-	obj = Horizons(id = str(planet_id), id_type = 'majorbody', 
-					location = None, epochs = Epochs)
-	eph = obj.ephemerides()
-
-	timelines = np.matrix((eph['datetime_str'],eph['RA'],
-			eph['DEC'])).transpose()
-
-	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'),timelines)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	
