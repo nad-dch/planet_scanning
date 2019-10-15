840299e82ab38161b8f2df17034582470f7bae83
nad-dch <55231643+nad-dch@users.noreply.github.com>
2019-10-15 15:08:51 +0200

    Add files via upload

--- planet_model.py
+++ planet_model.py
@@ -5,7 +5,7 @@ import os
 
 def brightness(T , d):
 
-	h = 6.2607015e-035
+	h = 6.2606014e-034
 	#v= 
 	B = 2*h*(v**2)*k*T/(c**2)*4*np.pi*(d**2)
 
@@ -21,21 +21,22 @@ def get_planet_timelines(planet_id, start_date, end_date,
 	opj = os.path.join
 
 	#convert the dates into the correct form YYYY-MM-DD
-	start_date = str(start_date)[:5]+'-'+str(start_date)[5:7]+'-'+str(start_date)[7:]
-	end_date = str(end_date)[:5]+'-'+str(end_date)[5:7]+'-'+str(end_date)[:7]
+	start_date = str(start_date)[:4]+'-'+str(start_date)[4:6]+'-'+str(start_date)[6:]
+	end_date = str(end_date)[:4]+'-'+str(end_date)[4:6]+'-'+str(end_date)[:6]
 	time_step = str(time_step)+'d'
 
 	Epochs = {'start':start_date,'stop':end_date,	
 			'step':time_step}
 
-	obj = Horizons(id = 'planet_id', id_type = 'majorbody', 
+	obj = Horizons(id = str(planet_id), id_type = 'majorbody', 
 					location = None, epochs = Epochs)
 
 	eph = obj.ephemerides()
 	timelines = np.matrix((eph['datetime_str'],eph['RA'],
 			eph['DEC'])).transpose()
 
-	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'),timelines)	
+	np.save(opj(filefolder, 'Planet_'+str(planet_id)+'_timelines'),timelines)
+
 
 
 
