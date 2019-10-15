095ec2beefc80fcedb9f5a13683f045c6e27e3ca
Jon E. Gudmundsson <jegudmunds@gmail.com>
2019-10-15 16:12:36 +0200

    further cleanup

--- example_jpl.py
+++ example_jpl.py
@@ -6,23 +6,25 @@ import astropy
 import datetime
 from astroquery.jplhorizons import Horizons
 
-def call_horizons(verbose=False):
+def call_horizons(verbose=False, time_interval='1h'):
 
-    Epochs={'start':"2018-10-01", 'stop':"2018-10-10", 'step':"1h"}
-
+    Epochs={'start':"2018-10-01", 'stop':"2018-10-10", 'step':time_interval}
     obj=Horizons(id='5', id_type='majorbody', location=None, epochs=Epochs)
     eph=obj.ephemerides()
 
-
+    # Converting relevant information into numpy arrays
     ra = np.array(eph['RA'])
     dec = np.array(eph['DEC'])
-    t = np.array(eph['datetime_jd'])#.to_datetime()
+    t = np.array(eph['datetime_jd'])
 
+    # Combining information into a dictionary
     data = {'ra':ra, 'dec':dec, 't':t}
 
+    # Writing data to a pickle file
     with open('horizons_output.pkl', 'wb') as handle:
         pickle.dump(data, handle)
 
+    # Printing out some useful debutting statements
     if verbose:
 
         print(eph['datetime_str'], eph['RA'], eph['DEC'])        
