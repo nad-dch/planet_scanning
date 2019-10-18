15331d7f20d67b2da2723642a2a9f5822e845d60
Konstantina Dachlythra <konstantina.dachlythra@fysik.su.se>
2019-10-18 17:40:21 +0200

    adding more files

--- noise.py
+++ noise.py
@@ -11,7 +11,7 @@ def white_noise(nsamp, sigma=1.0, mu=0.0):
 
 #method 1: analytical computations using scipy.fft
 
-def onef_noise(nsamp, sigma=1.0, fknee=20e-04):
+def onef_noise(nsamp, sigma=1.0, fknee=20e-03):
     '''
     Generate the desired 1/f noise tod
 
@@ -21,14 +21,13 @@ def onef_noise(nsamp, sigma=1.0, fknee=20e-04):
 	    		2 times the white noise floor
 		fs : the frequency sample 1/nsamp
     '''
-    fs = 20e-04*np.random_sample(nsamp)
-    fk = scipy.fftpack.fft(fs, n=None, axis=-1, overwrite_x=False)
-    phase = 2*np.pi*np.random_sample(nsamp)
-    C_k[0] = 0
+    C_k=[]
+    fs = 2*fknee*np.random.sample(nsamp)
+    fk = scipy.fftpack.fft(np.array(fs))
+    phase = 2*np.pi*np.random.sample(len(fk))
     C_k = 1/np.abs(fk)
-    C_k = C_k*np.exp(i*phase)
-    noise = scipy.fftpack.ifft(C_k, n=None, axis=-1, overwrite_x=False)
-
+    C_k = C_k*np.exp(1j*phase)
+    noise = scipy.fftpack.ifft(C_k)
     return noise
     #pass
 
@@ -38,7 +37,7 @@ def onef_noise(nsamp, sigma=1.0, fknee=20e-04):
 
 
 
-def psd_noise_model_no_h(f, white = 10e-05,fknee = 20e-03, alpha = 1, fmin = 0.1):
+def psd_noise_model_no_h(f = np.array(np.linspace(0,30e-03,100)), white = 10e-05,fknee = 20e-03, alpha =1, fmin = 0.001):
     '''
     The same as psd_noise_model, but with know hknee.
 
@@ -59,10 +58,8 @@ def psd_noise_model_no_h(f, white = 10e-05,fknee = 20e-03, alpha = 1, fmin = 0.1
     '''
     
     white = white**2
-    alpha = 2*alpha
-
-    return white * ((f**alpha + fknee**alpha) / (f**alpha + fmin**alpha))
-
+    #alpha = 2*alpha
+    return white * (fknee**alpha) / (f**alpha + fmin**alpha)
 
 def get_tod_from_psd(psd, check = True):
     '''
@@ -76,7 +73,7 @@ def get_tod_from_psd(psd, check = True):
 		timeline data : array of floats
 	'''
 	amplitude = np.sqrt(2*psd)
-	phase = np.random_sample()
+	phase = 2*np.pi*np.random.sample()
 	z = amplitude*np.exp(j*phase)
 	onef_tod = scipy.fftpack.ifft(z)
 
@@ -89,18 +86,88 @@ def get_tod_from_psd(psd, check = True):
 
 	return(onef_tod)
 
+# method 3: calculate a 1/f Fourier filter kernel amd interpolate it to the frequencies of the TOD
 
+def notch_kernel(f, fc, w, ns=5):
+    """
+    A very stupid (gaussian) notch filter kernel
+
+    Args:
+        f: array of frequencies at which to sample [Hz]
+        fc: the notch center [Hz]
+        w: the width of the notch [Hz]
+
+    Returns:
+        Array of kernel values at each frequency
+    """
+
+    f_low = fc - w*ns
+    f_hi = fc + w*ns
+    if (f_low<0):
+        f_low=0.0
+
+    krnl = np.ones_like(f)
+    cond = (f > f_low) & (f < f_hi)
+    krnl[cond] = 1.0 - np.exp(-0.5*((f[cond]-fc)/w)**2)
+    
+    if not np.all(np.isfinite(krnl)):
+        raise ValueError("Invalid value in notch_kernel")
+
+    return krnl
+
+def interp_kernel(freq , krnl, nsamp=None, fsamp=None, new_freq=None):
+    """
+    Interpolate a Fourier filter kernel.
+    It takes the kernel provided, which can be of arbitrary size
+    and interpolates it to the frequencies of the TOD samples.
+
+    Args:
+        freq: (array [arbitrary len]) frequencies of input kernel
+        krnl: (array [len of freq]) kernel, can be complex if you like
+        nsamp: length of frequency array for new samples
+        fsamp: sampling frequency of the tod.
+               Used with nsamp to generate new frequencies with fftfreq
+        new_freq: (array) array of new frequencies. Overrides nsamp, fsamp.
+
+    Returns:
+        Interpolated kernel, joy and happiness.
+
+    Note:
+        Currently assumes that freq > 0 and that krnl is conjugate-symmetric.
+    """
+    if new_freq is not None:
+        f = new_freq
+    elif nsamp is not None and fsamp is not None:
+        dt = 1./fsamp
+        #Generate the frequencies of the input TOD
+        #for even input, packing is 0 to nyquist, then -(nyquist-1) to 1
+        #for odd input, packing is 0 to nyquist, -nyquist to 1
+        f = np.fft.fftfreq(nsamp, dt)
+    else:
+        raise ValueError("Need either nsamp and fsamp, or new_freq")
+    
+    #Check that the tabulated frequencies are monotonic
+    if not np.all(np.diff(freq)):
+        raise ValueError("input kernel frequencies are not monotonic.")
+
+    #Interpolate the input kernel to the sample frequencies
+    #Note: might want to make this a polynomial rather than linear
+    #Interpolate the real and imaginary bits separately
+    k_re = np.interp(np.abs(f), freq, krnl.real)
+    k_im = np.interp(np.abs(f), freq, krnl.imag)
+    k_im[f < 0] *= (-1)         #Negative frequencies are conjugate 
+    return k_re + (1j)*k_im
+
+# test
 
 def test_noise_model():
-
-	f = np.array(np.linspace(0,0.040,10))
-	oise1 = psd_noise_model_no_h()
-	plt.plot(f,oise1)
+	f = np.array(np.linspace(0,30e-02,10))
+	noise_psd = psd_noise_model_no_h()
+	plt.plot(f,noise_psd)
 	plt.show()
-
-
-
-
+    noisy_timeline = get_tod_from_psd(noise_psd, check = True)
+    plt.plot(f,noisy_timeline)
+    plt.show()
 
 def main():
 
