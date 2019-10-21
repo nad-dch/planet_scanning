ad77730590df15cfa3df6fc5dbd8c2f66df676a2
jegudmunds <jegudmunds@gmail.com>
2019-10-21 10:41:09 +0100

    adding option 4

--- noise.py
+++ noise.py
@@ -10,7 +10,6 @@ def white_noise(nsamp, sigma=1.0, mu=0.0):
     return sigma*np.random.randn(nsamp)+mu
 
 #method 1: analytical computations using scipy.fft
-
 def onef_noise(nsamp, sigma=1.0, fknee=20e-03):
     '''
     Generate the desired 1/f noise tod
@@ -32,12 +31,9 @@ def onef_noise(nsamp, sigma=1.0, fknee=20e-03):
     #pass
 
 
-
 # method 2: using psd_noise_model_no_h and converting psd -->tod
-
-
-
-def psd_noise_model_no_h(f = np.array(np.linspace(0,30e-03,100)), white = 10e-05,fknee = 20e-03, alpha =1, fmin = 0.001):
+def psd_noise_model_no_h(f = np.array(np.linspace(0,30e-03,100)),
+    white = 10e-05,fknee = 20e-03, alpha =1, fmin = 0.001):
     '''
     The same as psd_noise_model, but with know hknee.
 
@@ -158,7 +154,88 @@ def interp_kernel(freq , krnl, nsamp=None, fsamp=None, new_freq=None):
     k_im[f < 0] *= (-1)         #Negative frequencies are conjugate
     return k_re + (1j)*k_im
 
-# test
+
+# Method 4: Jon's solution
+def extrap(x, xp, yp):
+    """
+    np.interp function with linear extrapolation
+    """
+    y = np.interp(x, xp, yp)
+    y = np.where(x<xp[0], yp[0]+(x-xp[0])*(yp[0]-yp[1])/(xp[0]-xp[1]), y)
+    y = np.where(x>xp[-1], yp[-1]+(x-xp[-1])*(yp[-1]-yp[-2])/(xp[-1]-xp[-2]), y)
+    return y
+
+def noise_psd(sigma0=1.0, fknee=0.020, alpha=1.5,
+    f=np.logspace(-3, 2, 100)):
+    '''
+    Generates a noise PSD from a three-paramater noise model
+    '''
+
+    psd = (sigma0 ** 2) * (1 + (fknee/f)**alpha)
+
+    return f, psd
+
+
+def noise_rel(nsamp, fsamp, fin=None, psdin=None):
+    '''
+    A generic noise generation function
+
+    Args:
+
+        nsamp: is the length of the output timestream mod(nsamp,2) = 0 (a must)
+        fsamp: is the sampling frequency [Hz]
+        fin: is the frequency values corresponding to the PSD
+        psdin: is the input psd
+
+    Returns:
+        noise: The noise timeline
+
+    '''
+
+    # Ensure that input parameters are interpreted as floats
+    nsamp = float(nsamp)
+    fsamp = float(fsamp)
+
+    # Using default values
+    if fin is None or psdin is None:
+        fin, psdin = noise_psd()
+
+    hnsamp = nsamp/2 # Half the length of the output timestream
+    dt = 1/fsamp
+    duration = nsamp*dt
+
+    # The frequency used as the abscissa for interpolation
+    f = np.zeros(nsamp)
+    f[0]= 0
+    f[1:(hnsamp+1)] = np.linspace(fsamp/nsamp, fsamp/2, hnsamp)
+    ftmp = f[1:hnsamp]
+    ftmp = ftmp[::-1] # Flipping the vector around
+    f[(hnsamp+1):] = ftmp
+
+    # Creating a new psd by interpolating the input psd
+    psdi = extrap(f,fin,psdin) # extrap comes from the pdata module
+    psdi[0] = 0 # force zero mean
+
+    # Gaussian random vector with zero mean and unit variance
+    np.random.seed()
+    w = np.random.randn(nsamp)
+
+    ntilde = np.zeros(nsamp, dtype='complex64')
+    ntilde[0] = w[0]*np.sqrt(psdi[0]/duration) # Zero by construction
+    ntilde[hnsamp] = w[-1]*np.sqrt(psdi[hnsamp]/duration)
+
+    # Creating the complex coefficients of the Fourier transform
+    ntilde[1:hnsamp] = (w[1:hnsamp]+w[(hnsamp+1):]*1j) \
+        *np.sqrt(psdi[1:hnsamp]/duration)/2.0
+
+    # The complex conjugate
+    ntmp = np.conjugate(ntilde[1:hnsamp])
+    ntmp = ntmp[::-1] # Flipping the vector around
+    ntilde[(hnsamp+1):] = ntmp
+    # ntilde is now conjugate symmetric
+    noise = np.fft.ifft(ntilde)*nsamp
+
+    return noise
 
 def test_noise_model():
 
@@ -170,9 +247,27 @@ def test_noise_model():
     plt.plot(f,noisy_timeline)
     plt.show()
 
+def test_option4():
+
+    nsamp = 100000.
+    fsamp = 50.
+    f, psd = noise_psd()
+    noise_out = noise_rel(nsamp, fsamp)
+
+    # Noise timestream
+    plt.plot(noise_out)
+    plt.savefig('img/noise_out.png', dpi=300)
+    plt.close()
+
+    # Noise PSD
+    plt.loglog(f, psd)
+    plt.savefig('img/noise_psd.png', dpi=300)
+    plt.close()
+
 def main():
 
-    test_noise_model()
+    # test_noise_model()
+    test_option4()
 
 if __name__ == '__main__':
     main()
